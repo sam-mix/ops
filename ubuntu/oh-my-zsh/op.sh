189e1557074b1f1d39558c388134f1834740b4fb
m <m@x.cn>
2021-06-15 09:21:37 +0800

    x

--- ubuntu/oh-my-zsh/op.sh
+++ ubuntu/oh-my-zsh/op.sh
@@ -1,10 +1,14 @@
 git clone https://gitee.com/mihongkun/ohmyzsh.git ~/.oh-my-zsh
+
+mv ~/.zshrc  ~/.zshrc.bak
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 git clone https://gitee.com/mihongkun/powerlevel10k.git ~/.oh-my-zsh/themes/powerlevel10k
 git clone https://gitee.com/mihongkun/zsh-autosuggestions.git ~/.oh-my-zsh/plugins/zsh-autosuggestions
 git clone https://gitee.com/mihongkun/zsh-syntax-highlighting.git ~/.oh-my-zsh/plugins/zsh-syntax-highlighting
 
-vi ~/.zsh
+vi ~/.zshrc
+or
+code ~/.zshrc
 robbyrussell -> powerlevel10k/powerlevel10k
 
 plugins=(git) -> 
@@ -16,4 +20,6 @@ plugins=(
 )
 
 
-
+install nerd fonts:
+www.nerdfonts.com
+www.programmingfonts.org
